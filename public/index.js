c929a29c728c6799a3f83f5ad5c1c6f99ed516d4
Matt Kosarek <matt.kosarek@canonical.com>
2026-02-23 07:48:55 -0500

    Improve sitemap html

--- public/index.js
+++ public/index.js
@@ -1,38 +1,6 @@
 
 function main() {
-    var themeSelector = document.getElementById('theme_selector');
-    themeSelector.classList.remove('hidden');
-
-    imageCallbacks();
-}
-
-function imageCallbacks() {
-  const imageContainer = document.getElementById('image_container');
-  
-    function onImageClicked() {
-        var background = document.createElement('div'); // 
-        background.classList.add('image_item_expanded_container');
-        var clone = this.cloneNode(true);
-        clone.classList.add('expanded');
-        background.append(clone);
-        clone.addEventListener('click', function(event) { event.stopPropagation(); });
-        background.addEventListener('click', function() { background.remove(); });
-        window.addEventListener('keydown', function(e) { if (e.key === 'Escape') background.remove(); });
-        document.body.parentElement.prepend(background);
-    }
-
-    function setImageClicked() {
-        for (var i = 0; i < numImages; i++) {
-            var image = imageList[i];
-            image.addEventListener('click', onImageClicked);
-        }
-    } 
-
-    // Set up on image clicked
-    var imageList = imageContainer.children,
-        numImages = imageContainer.children.length;
-
-    setImageClicked();
+   
 }
 
 main();
--- themes/src/webgl_context.cpp
+++ themes/src/webgl_context.cpp
@@ -1,46 +1,46 @@
 #include "webgl_context.h"
 #include <cstdio>
 
+EM_BOOL onResize(int eventType, const EmscriptenUiEvent *uiEvent,
+                 void *userData) {
+  WebglContext *context = (WebglContext *)userData;
 
-EM_BOOL onResize(int eventType, const EmscriptenUiEvent *uiEvent, void *userData) {
-	WebglContext* context = (WebglContext*)userData;
+  f64 inWidth, inHeight;
+  emscripten_get_element_css_size(context->query, &inWidth, &inHeight);
 
-    f64 inWidth, inHeight;
-    emscripten_get_element_css_size(context->query, &inWidth, &inHeight);
+  context->width = static_cast<f32>(inWidth);
+  context->height = static_cast<f32>(inHeight);
+  emscripten_set_canvas_element_size(context->query, context->width, context->height);
 
-    context->width = static_cast<f32>(inWidth);
-    context->height = static_cast<f32>(inHeight);
-
-    return true;
+  return true;
 }
 
-void WebglContext::init(const char* inQuery) {
-    strcpy(query, inQuery);
-    f64 inWidth, inHeight;
-    emscripten_get_element_css_size(query, &inWidth, &inHeight);
-    width = static_cast<f32>(inWidth);
-    height = static_cast<f32>(inHeight);
-    emscripten_set_canvas_element_size( query, width, height);
-    
-    EmscriptenWebGLContextAttributes attrs;
-    emscripten_webgl_init_context_attributes(&attrs);
+void WebglContext::init(const char *inQuery) {
+  strcpy(query, inQuery);
+  f64 inWidth, inHeight;
+  emscripten_get_element_css_size(query, &inWidth, &inHeight);
+  width = static_cast<f32>(inWidth);
+  height = static_cast<f32>(inHeight);
+  emscripten_set_canvas_element_size(query, width, height);
 
-    attrs.enableExtensionsByDefault = 1;
-    attrs.majorVersion = 3;
-    attrs.minorVersion = 0;
+  EmscriptenWebGLContextAttributes attrs;
+  emscripten_webgl_init_context_attributes(&attrs);
 
-    context = emscripten_webgl_create_context(query, &attrs);
-    makeCurrentContext();
+  attrs.enableExtensionsByDefault = 1;
+  attrs.majorVersion = 3;
+  attrs.minorVersion = 0;
 
-    glClearColor(0, 0, 0, 0.0f);
+  context = emscripten_webgl_create_context(query, &attrs);
+  makeCurrentContext();
 
-    emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, this, false, onResize);
+  glClearColor(0, 0, 0, 0.0f);
+
+  emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, this, false,
+                                 onResize);
 };
 
 void WebglContext::makeCurrentContext() {
-    emscripten_webgl_make_context_current(context);
+  emscripten_webgl_make_context_current(context);
 };
 
-void WebglContext::destroy() {
-    emscripten_webgl_destroy_context(context);
-}
+void WebglContext::destroy() { emscripten_webgl_destroy_context(context); }
